bf7949943ee0a83a6eedb229974216bb8885f534
Jordan Harband <ljharb@gmail.com>
2023-09-04 21:36:08 -0700

    [Deps] update `define-properties`, `es-abstract`, `get-intrinsic`

--- implementation.js
+++ implementation.js
@@ -4,13 +4,13 @@ var GetIntrinsic = require('get-intrinsic');
 var callBound = require('call-bind/callBound');
 var $TypeError = GetIntrinsic('%TypeError%');
 
-var Call = require('es-abstract/2022/Call');
-var Get = require('es-abstract/2022/Get');
-var HasProperty = require('es-abstract/2022/HasProperty');
-var IsCallable = require('es-abstract/2022/IsCallable');
-var LengthOfArrayLike = require('es-abstract/2022/LengthOfArrayLike');
-var ToObject = require('es-abstract/2022/ToObject');
-var ToString = require('es-abstract/2022/ToString');
+var Call = require('es-abstract/2023/Call');
+var Get = require('es-abstract/2023/Get');
+var HasProperty = require('es-abstract/2023/HasProperty');
+var IsCallable = require('es-abstract/2023/IsCallable');
+var LengthOfArrayLike = require('es-abstract/2023/LengthOfArrayLike');
+var ToObject = require('es-abstract/2023/ToObject');
+var ToString = require('es-abstract/2023/ToString');
 
 var isString = require('is-string');
 
